2db6ba6cc3d9bd4f5ce5810f021910310ec625ba
MahedyHasan0017 <mhdyhasanpust017@gmail.com>
2024-07-16 14:55:41 +0600

    first-commit-configuring database env app_nam authentication pack breeze and creating user for admin teacher and student role

---
+++ notes/1.install-breeze-auth-package.php
@@ -0,0 +1,16 @@
+composer require laravel/breeze --dev
+php artisan breeze:install
+php artisan migrate
+npm install
+npm run dev
+
+php artisan serve 
+
+now we creating some users of different roles 
+
+admin 
+teacher
+student
+
+
+
