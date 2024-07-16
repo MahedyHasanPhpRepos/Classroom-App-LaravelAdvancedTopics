a8cd26511caf0267a8caba59883ef57656eac86e
MahedyHasan0017 <mhdyhasanpust017@gmail.com>
2024-07-16 15:51:02 +0600

    adding class_types and scheduled_classes table and establish approprate relation between them and creating methods for access related data

--- notes/1.install-breeze-auth-package.php
+++ notes/1.install-breeze-auth-package.php
@@ -12,5 +12,12 @@ admin
 teacher
 student
 
+creating table for users/teacher , class_types/course name , scheduled_classes/time_for_the_class 
+
+
+scheduled_classes table will manage which teacher , which course in which time 
+
+one teacher can take many scheduled_classes so one to many relationship 
+one course can be in many scheduled_classes so one to many relationship 
 
 
